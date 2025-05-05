796a2758e4fe2374e987e760eb6da1109add4402
laozhu <lemurzjb@gmail.com>
2025-05-05 20:42:53 +0800

    fix security issues

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
     )
     
     # CORS配置
-    BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:3000", "http://localhost:8000"]
+    BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:3000", "http://localhost:8000", os.getenv("FRONTEND_URL", "http://localhost:3000")]
     
     # OAuth配置
     GOOGLE_CLIENT_ID: Optional[str] = os.getenv("GOOGLE_CLIENT_ID", "")
@@ -31,11 +31,11 @@ class Settings(BaseSettings):
     FRONTEND_URL: str = "http://localhost:3000"
     
     # 邮件配置 - 163邮箱
-    SMTP_USER: str = os.getenv("SMTP_USER", "mouse0407@163.com")
-    SMTP_PASSWORD: str = os.getenv("SMTP_PASSWORD", "EFykXumtcnfxHzAm")  # 注意：使用授权码而非邮箱密码
-    SMTP_SERVER: str = os.getenv("SMTP_SERVER","smtp.163.com")
-    SMTP_PORT: int = os.getenv("SMTP_PORT", 465)  # 163邮箱推荐使用SSL加密的465端口
-    SMTP_SSL: bool = os.getenv("SMTP_SSL", "True").lower() == "true"  # 启用SSL加密
+    SMTP_USER: str = os.getenv("SMTP_USER", "")
+    SMTP_PASSWORD: str = os.getenv("SMTP_PASSWORD", "")  # 注意：使用授权码而非邮箱密码
+    SMTP_SERVER: str = os.getenv("SMTP_SERVER","")
+    SMTP_PORT: int = os.getenv("SMTP_PORT")  # 163邮箱推荐使用SSL加密的465端口
+    SMTP_SSL: bool = os.getenv("SMTP_SSL").lower() == "true"  # 启用SSL加密
     MAIL_FROM_NAME: str = "MyDiary应用"  # 发件人名称
 
     # MySQL SSL配置
